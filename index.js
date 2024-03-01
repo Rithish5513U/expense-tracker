f705696f6f7e85ddef2d46fa781806a40e78530d
Rithish5513U <rithish.satish@gmail.com>
2024-03-01 14:05:06 +0530

    initial

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ app.use("/",static);
 
 app.get("/hi",(req,res)=>{
     console.log(req.query)
-    //res.send("hello World")
-    //http://localhost:4000/hi?phone=12
-    /**
-     * host: localhost/127.0.0.1
-     * port: 4000
-     * path:/hi
-     * ?key=value
-     */
     res.json(req.query)
 })
 //  })
